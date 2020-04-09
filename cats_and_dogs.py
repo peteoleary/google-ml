b7405e949c45e36b3660f19ee57f631f5c27e8e8
Peter O'Leary <pete@timelight.com>
2020-04-09 14:31:10 -0700

    Finished exercise 2 and added lots of flags

--- cats_and_dogs.py
+++ cats_and_dogs.py
@@ -1,4 +1,12 @@
+import sys
 import os
+
+def check_flag(flag):
+    if flag in sys.argv:
+        print("check_flag(%s) is true" % flag, flush=True)
+        return True
+    else:
+        return False
 
 base_dir = './cats_and_dogs_filtered'
 train_dir = os.path.join(base_dir, 'train')
@@ -28,35 +36,41 @@ print('total training dog images:', len(os.listdir(train_dogs_dir)))
 print('total validation cat images:', len(os.listdir(validation_cats_dir)))
 print('total validation dog images:', len(os.listdir(validation_dogs_dir)))
 
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
-# Parameters for our graph; we'll output images in a 4x4 configuration
-nrows = 4
-ncols = 4
+def show_some_images():
+    # Parameters for our graph; we'll output images in a 4x4 configuration
+    nrows = 4
+    ncols = 4
 
-# Index for iterating over images
-pic_index = 0
+    # Index for iterating over images
+    pic_index = 0
 
-# Set up matplotlib fig, and size it to fit 4x4 pics
-fig = plt.gcf()
-fig.set_size_inches(ncols * 4, nrows * 4)
+    # Set up matplotlib fig, and size it to fit 4x4 pics
+    fig = plt.gcf()
+    fig.set_size_inches(ncols * 4, nrows * 4)
 
-pic_index += 8
-next_cat_pix = [os.path.join(train_cats_dir, fname) 
-                for fname in train_cat_fnames[pic_index-8:pic_index]]
-next_dog_pix = [os.path.join(train_dogs_dir, fname) 
-                for fname in train_dog_fnames[pic_index-8:pic_index]]
+    pic_index += 8
+    next_cat_pix = [os.path.join(train_cats_dir, fname) 
+                    for fname in train_cat_fnames[pic_index-8:pic_index]]
+    next_dog_pix = [os.path.join(train_dogs_dir, fname) 
+                    for fname in train_dog_fnames[pic_index-8:pic_index]]
 
-for i, img_path in enumerate(next_cat_pix+next_dog_pix):
-  # Set up subplot; subplot indices start at 1
-  sp = plt.subplot(nrows, ncols, i + 1)
-  sp.axis('Off') # Don't show axes (or gridlines)
+    for i, img_path in enumerate(next_cat_pix+next_dog_pix):
+      # Set up subplot; subplot indices start at 1
+      sp = plt.subplot(nrows, ncols, i + 1)
+      sp.axis('Off') # Don't show axes (or gridlines)
 
-  img = mpimg.imread(img_path)
-  plt.imshow(img)
+      img = mpimg.imread(img_path)
+      plt.imshow(img)
 
-plt.show()
+    plt.show()
+    
+if check_flag('show_images'):
+    show_some_images()
 
 import tensorflow
 
@@ -88,6 +102,10 @@ x = layers.Flatten()(x)
 # Create a fully connected layer with ReLU activation and 512 hidden units
 x = layers.Dense(512, activation='relu')(x)
 
+if check_flag('dropout'):
+    # Add a dropout rate of 0.5
+    x = layers.Dropout(0.5)(x)
+
 # Create output layer with a single node and sigmoid activation
 output = layers.Dense(1, activation='sigmoid')(x)
 
@@ -108,94 +126,114 @@ model.compile(loss='binary_crossentropy',
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 # All images will be rescaled by 1./255
-train_datagen = ImageDataGenerator(rescale=1./255)
+
+if check_flag('augment'):
+    train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        rotation_range=40,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True,)
+else:
+    train_datagen = ImageDataGenerator(rescale=1./255)
+    
 val_datagen = ImageDataGenerator(rescale=1./255)
+
+training_batch_size = 20
 
 # Flow training images in batches of 20 using train_datagen generator
 train_generator = train_datagen.flow_from_directory(
         train_dir,  # This is the source directory for training images
         target_size=(150, 150),  # All images will be resized to 150x150
-        batch_size=20,
+        batch_size=training_batch_size,
         # Since we use binary_crossentropy loss, we need binary labels
         class_mode='binary')
+
+validation_batch_size = 20
 
 # Flow validation images in batches of 20 using val_datagen generator
 validation_generator = val_datagen.flow_from_directory(
         validation_dir,
         target_size=(150, 150),
-        batch_size=20,
+        batch_size=validation_batch_size,
         class_mode='binary')
 
-print("training the model...", flush=True)
-# train the model
+training_steps = 100
+validation_steps = 50
+
+print("training the model total_training_size=%d total_validation_size=%d..." % (training_batch_size * training_steps, validation_batch_size * validation_steps), flush=True)
+
 history = model.fit_generator(
       train_generator,
-      steps_per_epoch=100,  # 2000 images = batch_size * steps
-      epochs=15,
+      steps_per_epoch=training_steps,  # 2000 images = batch_size * steps
+      epochs=30,
       validation_data=validation_generator,
-      validation_steps=50,  # 1000 images = batch_size * steps
-      verbose=2)
+      validation_steps=validation_steps,  # 1000 images = batch_size * steps
+      verbose=1)
 
 print("finished training...", flush=True)
 
 # TODO: save model here?
 
-import numpy as np
-import random
-from tensorflow.keras.preprocessing.image import img_to_array, load_img
+if check_flag('show_intermediate'):
+    import numpy as np
+    import random
+    from tensorflow.keras.preprocessing.image import img_to_array, load_img
 
-# Let's define a new Model that will take an image as input, and will output
-# intermediate representations for all layers in the previous model after
-# the first.
-successive_outputs = [layer.output for layer in model.layers[1:]]
-visualization_model = Model(img_input, successive_outputs)
+    # Let's define a new Model that will take an image as input, and will output
+    # intermediate representations for all layers in the previous model after
+    # the first.
+    successive_outputs = [layer.output for layer in model.layers[1:]]
+    visualization_model = Model(img_input, successive_outputs)
 
-# Let's prepare a random input image of a cat or dog from the training set.
-cat_img_files = [os.path.join(train_cats_dir, f) for f in train_cat_fnames]
-dog_img_files = [os.path.join(train_dogs_dir, f) for f in train_dog_fnames]
-img_path = random.choice(cat_img_files + dog_img_files)
+    # Let's prepare a random input image of a cat or dog from the training set.
+    cat_img_files = [os.path.join(train_cats_dir, f) for f in train_cat_fnames]
+    dog_img_files = [os.path.join(train_dogs_dir, f) for f in train_dog_fnames]
+    img_path = random.choice(cat_img_files + dog_img_files)
 
-img = load_img(img_path, target_size=(150, 150))  # this is a PIL image
-x = img_to_array(img)  # Numpy array with shape (150, 150, 3)
-x = x.reshape((1,) + x.shape)  # Numpy array with shape (1, 150, 150, 3)
+    img = load_img(img_path, target_size=(150, 150))  # this is a PIL image
+    x = img_to_array(img)  # Numpy array with shape (150, 150, 3)
+    x = x.reshape((1,) + x.shape)  # Numpy array with shape (1, 150, 150, 3)
 
-# Rescale by 1/255
-x /= 255
+    # Rescale by 1/255
+    x /= 255
 
-# Let's run our image through our network, thus obtaining all
-# intermediate representations for this image.
-successive_feature_maps = visualization_model.predict(x)
+    # Let's run our image through our network, thus obtaining all
+    # intermediate representations for this image.
+    successive_feature_maps = visualization_model.predict(x)
 
-# These are the names of the layers, so can have them as part of our plot
-layer_names = [layer.name for layer in model.layers]
+    # These are the names of the layers, so can have them as part of our plot
+    layer_names = [layer.name for layer in model.layers]
 
-# Now let's display our representations
-for layer_name, feature_map in zip(layer_names, successive_feature_maps):
-  if len(feature_map.shape) == 4:
-    # Just do this for the conv / maxpool layers, not the fully-connected layers
-    n_features = feature_map.shape[-1]  # number of features in feature map
-    # The feature map has shape (1, size, size, n_features)
-    size = feature_map.shape[1]
-    # We will tile our images in this matrix
-    display_grid = np.zeros((size, size * n_features))
-    for i in range(n_features):
-      # Postprocess the feature to make it visually palatable
-      x = feature_map[0, :, :, i]
-      x -= x.mean()
-      x /= x.std()
-      x *= 64
-      x += 128
-      x = np.clip(x, 0, 255).astype('uint8')
-      # We'll tile each filter into this big horizontal grid
-      display_grid[:, i * size : (i + 1) * size] = x
-    # Display the grid
-    scale = 20. / n_features
-    plt.figure(figsize=(scale * n_features, scale))
-    plt.title(layer_name)
-    plt.grid(False)
-    plt.imshow(display_grid, aspect='auto', cmap='viridis')
-    
-print("done calculating display...", flush=True)
+    # Now let's display our representations
+    for layer_name, feature_map in zip(layer_names, successive_feature_maps):
+      if len(feature_map.shape) == 4:
+        # Just do this for the conv / maxpool layers, not the fully-connected layers
+        n_features = feature_map.shape[-1]  # number of features in feature map
+        # The feature map has shape (1, size, size, n_features)
+        size = feature_map.shape[1]
+        # We will tile our images in this matrix
+        display_grid = np.zeros((size, size * n_features))
+        for i in range(n_features):
+          # Postprocess the feature to make it visually palatable
+          x = feature_map[0, :, :, i]
+          x -= x.mean()
+          x /= x.std()
+          x *= 64
+          x += 128
+          x = np.clip(x, 0, 255).astype('uint8')
+          # We'll tile each filter into this big horizontal grid
+          display_grid[:, i * size : (i + 1) * size] = x
+        # Display the grid
+        scale = 20. / n_features
+        plt.figure(figsize=(scale * n_features, scale))
+        plt.title(layer_name)
+        plt.grid(False)
+        plt.imshow(display_grid, aspect='auto', cmap='viridis')
+
+    print("done calculating display...", flush=True)
     
 # Retrieve a list of accuracy results on training and validation data
 # sets for each training epoch
